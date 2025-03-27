47a4db1f4bab0bcffbe5b056b05237a8bce204d5
Hoyasumii <alanreisanjo@gmail.com>
2025-03-27 20:23:57 -0300

    Ver número de operações

--- better-quicksort.ts
+++ better-quicksort.ts
@@ -5,13 +5,20 @@ type CompareToMethod<ContentType> = (
   item2: ContentType
 ) => -1 | 0 | 1;
 
+const shared = {
+  operations: 0,
+};
+
 export function betterQuicksort<ContentType>(
   data: Array<ContentType>,
-  compareTo: CompareToMethod<ContentType>
+  compareTo: CompareToMethod<ContentType>,
+  sharedData: typeof shared = shared
 ): Array<ContentType> {
   if (data.length < 2) {
     return data;
   }
+
+  sharedData.operations += 1;
 
   const pivot: ContentType = data[Math.floor(data.length / 2)];
 
@@ -42,20 +49,18 @@ const finalGenerateContentTime = Date.now();
 
 const initialTime = Date.now();
 
-console.log(
-  betterQuicksort<string>(content, (item1, item2) => {
-    const parsedItem1 = item1.split("").map((item) => item.charCodeAt(0));
-    const parsedItem2 = item2.split("").map((item) => item.charCodeAt(0));
+betterQuicksort<string>(content, (item1, item2) => {
+  const parsedItem1 = item1.split("").map((item) => item.charCodeAt(0));
+  const parsedItem2 = item2.split("").map((item) => item.charCodeAt(0));
 
-    for (let i = 0; i < Math.min(parsedItem1.length, parsedItem2.length); i++) {
-      if (parsedItem1[i] !== parsedItem2[i]) {
-        return (parsedItem1[i] - parsedItem2[i]) as 0;
-      }
+  for (let i = 0; i < Math.min(parsedItem1.length, parsedItem2.length); i++) {
+    if (parsedItem1[i] !== parsedItem2[i]) {
+      return (parsedItem1[i] - parsedItem2[i]) as 0;
     }
+  }
 
-    return (parsedItem1.length - parsedItem2.length) as 0;
-  })
-);
+  return (parsedItem1.length - parsedItem2.length) as 0;
+});
 
 console.log(
   `Time to Generate an Random Array<string>: ${
@@ -63,4 +68,5 @@ console.log(
   }ms`
 );
 console.log(`Time to execute: ${Date.now() - initialTime}ms`);
+console.log(shared.operations);
 // 61ms NODE
