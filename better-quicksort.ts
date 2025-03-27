d8a0cf8c0437e38666c466739cf4660efd29e8c1
Hoyasumii <alanreisanjo@gmail.com>
2025-03-27 19:52:57 -0300

    Anotações para benchmark

--- better-quicksort.ts
+++ better-quicksort.ts
@@ -1,3 +1,5 @@
+import { faker } from "@faker-js/faker";
+
 type CompareToMethod<ContentType> = (
   item1: ContentType,
   item2: ContentType
@@ -13,25 +15,52 @@ export function betterQuicksort<ContentType>(
 
   const pivot: ContentType = data[Math.floor(data.length / 2)];
 
-  const min: Array<ContentType> = data.filter((item) => compareTo(item, pivot) < 0);
-  const equal: Array<ContentType> = data.filter((item) => compareTo(item, pivot) === 0);
-  const max: Array<ContentType> = data.filter((item) => compareTo(item, pivot) > 0);
+  const min: Array<ContentType> = data.filter(
+    (item) => compareTo(item, pivot) < 0
+  );
+  const equal: Array<ContentType> = data.filter(
+    (item) => compareTo(item, pivot) === 0
+  );
+  const max: Array<ContentType> = data.filter(
+    (item) => compareTo(item, pivot) > 0
+  );
 
-  return [...betterQuicksort(min, compareTo), ...equal, ...betterQuicksort(max, compareTo)];
+  return [
+    ...betterQuicksort(min, compareTo),
+    ...equal,
+    ...betterQuicksort(max, compareTo),
+  ];
 }
 
-let content: Array<string> = ["José", "Alan", "Lucas", "Albérico", "João", "Pedro", "Abner", "Júlia"];
+const initialGenerateContentTime = Date.now();
 
-console.log(betterQuicksort<string>(content, (item1, item2) => {
-  const parsedItem1 = item1.split("").map(item => item.charCodeAt(0));
-  const parsedItem2 = item2.split("").map(item => item.charCodeAt(0));
+let content: Array<string> = Array.from({ length: 100_000 }).map(() =>
+  faker.person.fullName()
+);
 
-  for (let i = 0; i < Math.min(parsedItem1.length, parsedItem2.length); i++) {
-    if (parsedItem1[i] !== parsedItem2[i]) {
-      return parsedItem1[i] - parsedItem2[i] as 0;
+const finalGenerateContentTime = Date.now();
+
+const initialTime = Date.now();
+
+console.log(
+  betterQuicksort<string>(content, (item1, item2) => {
+    const parsedItem1 = item1.split("").map((item) => item.charCodeAt(0));
+    const parsedItem2 = item2.split("").map((item) => item.charCodeAt(0));
+
+    for (let i = 0; i < Math.min(parsedItem1.length, parsedItem2.length); i++) {
+      if (parsedItem1[i] !== parsedItem2[i]) {
+        return (parsedItem1[i] - parsedItem2[i]) as 0;
+      }
     }
-  }
 
-  return parsedItem1.length - parsedItem2.length as 0;
-}));
+    return (parsedItem1.length - parsedItem2.length) as 0;
+  })
+);
 
+console.log(
+  `Time to Generate an Random Array<string>: ${
+    finalGenerateContentTime - initialGenerateContentTime
+  }ms`
+);
+console.log(`Time to execute: ${Date.now() - initialTime}ms`);
+// 61ms NODE
